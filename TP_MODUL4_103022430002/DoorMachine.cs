f60debfc3b710112b4069482980309da300941a5
raflyputra190 <raflyputra190@gmail.com>
2026-03-16 01:31:47 +0700

    git add . git commit -m “menambahkan class DoorMachine” git push -u origin master

---
+++ TP_MODUL4_103022430002/DoorMachine.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+class DoorMachine
+{
+    private enum State
+    {
+        Terkunci,
+        Terbuka
+    }
+
+    private State currentState;
+
+    public DoorMachine()
+    {
+        currentState = State.Terkunci;
+        TampilkanStatus();
+    }
+
+    public void BukaPintu()
+    {
+        if (currentState == State.Terkunci)
+        {
+            currentState = State.Terbuka;
+        }
+
+        TampilkanStatus();
+    }
+
+    public void KunciPintu()
+    {
+        if (currentState == State.Terbuka)
+        {
+            currentState = State.Terkunci;
+        }
+
+        TampilkanStatus();
+    }
+
+    private void TampilkanStatus()
+    {
+        if (currentState == State.Terkunci)
+        {
+            Console.WriteLine("Pintu terkunci");
+        }
+        else
+        {
+            Console.WriteLine("Pintu tidak terkunci");
+        }
+    }
+}
